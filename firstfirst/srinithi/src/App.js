ec14d6aef9fde9f33ddac979f5b65290cb0d74cb
Srinithi1916 <srinithisrinithi09@gmail.com>
2023-11-07 12:45:10 +0530

    components

--- firstfirst/srinithi/src/App.js
+++ firstfirst/srinithi/src/App.js
@@ -2,7 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 import { Message } from './Message';
 import {ListElements} from './ListElements';
-
+import Main from './components/Main';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 function App() {
   //we can return only one element in the return statement.
   return(
