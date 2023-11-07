33ffc4bf625420b9474f9716a3119896d1d33b66
Srinithi1916 <srinithisrinithi09@gmail.com>
2023-11-07 12:22:54 +0530

    commit components

--- firstfirst/srinithi/src/App.js
+++ firstfirst/srinithi/src/App.js
@@ -1,27 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { Message } from './Message';
+import {ListElements} from './ListElements';
 
 function App() {
-  return (
-  /*
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>*/
-      
-  );
+  //we can return only one element in the return statement.
+  return(
+  <div>
+    <Message></Message>
+    <Message></Message>
+    <ListElements></ListElements>
+    <Main></Main>
+    <Navbar></Navbar>
+    <Footer></Footer>
+  </div>
+  )
 }
-
 export default App;
